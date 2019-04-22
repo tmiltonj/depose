58afcbcd38818f1aee43b0997b704b9d2e6cf4cf
tmiltonj <thabo.miltonjonas@gmail.com>
2019-04-22 21:05:22 +0800

    Moved Player from controller to model

--- depose/model.py
+++ depose/model.py
@@ -8,6 +8,7 @@ class Card(Enum):
     MEDIC = auto
     DIPLOMAT = auto
 
+
 class Deck():
     def __init__(self):
         self.cards = []
@@ -20,8 +21,85 @@ class Deck():
         self.cards.remove(selection)
         return selection
 
+
 class Player():
-    def __init__(self, name):
+    def __init__(self, name, deck=None, game=None, action_factory=None):
         self.name = name
-        self.coins = 0
-        self.cards = []
+        self._coins = 0
+        self._cards = []
+
+        self.deck = deck
+        self.game = game
+        self.action_factory = action_factory
+
+    @property
+    def coins(self):
+        return self._coins
+
+    @coins.setter
+    def coins(self, value):
+        self._coins = max(0, value)
+
+    @property
+    def cards(self):
+        return self._cards
+
+    def add_card(self, card):
+        self._cards.append(card)
+
+    def __str__(self):
+        return "{} (Coins: {}, Cards: {})".format(self.name, self._coins, len(self._cards))
+
+    def draw_cards(self, number=1):
+        for _ in range(number):
+            self._cards.append(self.deck.get())
+
+    def return_card(self, card=None):
+        if card is None:
+            card = '' # Choose a card
+            #self.game.wait_for_input(options=self._cards)
+        
+        if card not in self._cards:
+            raise ValueError("cannot find {}".format(card))
+
+        self.deck.add(card)
+        self._cards.remove(card)
+
+    def reveal_card(self, card=None):
+        if card is None:
+            card = '' # Choose a card
+            #self.game.wait_for_input(options=self._cards)
+
+        if card not in self._cards:
+            raise ValueError("Cannot find {}".format(card))
+
+        return card
+
+    def lose_life(self, card=None):
+        if card is None:
+            card = '' # Choose a card
+            #self.game.wait_for_input(options=self._cards)
+        
+        if card not in self._cards:
+            raise ValueError("Cannot find {}".format(card))
+
+        self._cards.remove(card)
+        return card
+
+    def choose_action(self):
+        #action_list = self._get_action_list()
+        #self.game.wait_for_input(action_list)
+        #self.action_factory.create()
+        return
+
+    def choose_target(self, targets):
+        #target = self.game.wait_for_input(targets)
+        return
+
+    def ask_to_counter(self, action):
+        #choice = self.game.wait_for_input('')
+        return
+
+    def ask_to_challenge(self, action):
+        #choice = self.game.wait_for_input('')
+        return
